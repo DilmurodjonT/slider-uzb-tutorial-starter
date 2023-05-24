0e8dbec04ae1ba67e558483c4d4281defbd11919
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-05-24 17:15:26 +0500

    forst commit

--- js/script.js
+++ js/script.js
@@ -8,14 +8,37 @@ window.addEventListener("DOMContentLoaded", () => {
     current = document.querySelector("#current"),
     total = document.querySelector("#total");
 
+  let slideIndex = 1;
+  let offset = 0;
+
   slidesField.style.width = 100 * slides.length + "%";
 
   slidesField.style.display = "flex";
-  slidesField.style.transition = ".5s ease";
+  slidesField.style.transition = "0.5s all";
   slidesWrapper.style.overflow = "hidden";
 
   slides.forEach((slide) => {
     slide.style.width = width;
+  });
+
+  next.addEventListener("click", () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+  });
+
+  prev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
   });
 });
 
